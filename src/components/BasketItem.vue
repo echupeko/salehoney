3da17fcb67ae2b3608bb39b246ef78a0d2d64b7a
Евгений <echupeko@gmail.com>
2021-01-18 16:59:07 +0700

    update

--- src/components/BasketItem.vue
+++ src/components/BasketItem.vue
@@ -1,10 +1,20 @@
 <template>
-<div></div>
+  <div>
+    <h3 class="item-title"></h3>
+    <div class="counter">
+      <input type="submit" value="-">
+      <input type="number" step="1" min="1" max="20">
+      <input type="submit" value="+">
+    </div>
+    <h3 class="item-price"></h3>
+    <button class="remove-item"></button>
+  </div>
 </template>
 
 <script>
+
 export default {
-name: "BasketItem"
+  name: "BasketItem",
 }
 </script>
 
